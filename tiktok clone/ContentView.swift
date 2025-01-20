99b2fc6b9c93089f54772f96aba0cac55e861dcf
Akmal-1993 <akmalrixsiboyev4@gmail.com>
2025-01-20 16:37:15 +0500

    Initial Commit

---
+++ tiktok clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tiktok clone
+//
+//  Created by Akmal on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+           
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tiktok clone/tiktok_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktok_cloneApp.swift
+//  tiktok clone
+//
+//  Created by Akmal on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct tiktok_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
